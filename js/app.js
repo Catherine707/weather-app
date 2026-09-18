14c65e0c1bd95ce60cf1117fda7385204bb0df3d
Catherine707 <catherinecoti18@gmail.com>
2026-09-17 23:03:31 -0600

    feat: completar descripciones de códigos WMO

--- js/app.js
+++ js/app.js
@@ -110,15 +110,34 @@ function displayWeather(name, country, data) {
 
 function getWeatherDescription(code) {
     const codes = {
-        0: 'Despejado ',
-        1: 'Principalmente despejado ',
-        2: 'Parcialmente nublado ',
-        3: 'Nublado ',
-        45: 'Neblina ',
-        51: 'Llovizna ligera ',
-        61: 'Lluvia ligera ',
-        71: 'Nieve ligera ',
-        95: 'Tormenta eléctrica'
+        0: 'Despejado',
+        1: 'Principalmente despejado',
+        2: 'Parcialmente nublado',
+        3: 'Nublado',
+        45: 'Niebla',
+        48: 'Niebla con escarcha',
+        51: 'Llovizna ligera',
+        53: 'Llovizna moderada',
+        55: 'Llovizna intensa',
+        56: 'Llovizna helada ligera',
+        57: 'Llovizna helada intensa',
+        61: 'Lluvia ligera',
+        63: 'Lluvia moderada',
+        65: 'Lluvia intensa',
+        66: 'Lluvia helada ligera',
+        67: 'Lluvia helada intensa',
+        71: 'Nevada ligera',
+        73: 'Nevada moderada',
+        75: 'Nevada intensa',
+        77: 'Granos de nieve',
+        80: 'Chubascos ligeros',
+        81: 'Chubascos moderados',
+        82: 'Chubascos intensos',
+        85: 'Chubascos de nieve ligeros',
+        86: 'Chubascos de nieve intensos',
+        95: 'Tormenta eléctrica',
+        96: 'Tormenta eléctrica con granizo ligero',
+        99: 'Tormenta eléctrica con granizo intenso'
     };
     return codes[code] || `Condición (WMO: ${code})`;
 }
